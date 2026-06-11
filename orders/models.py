6af6bfa710fec5a29c6c50df15a2e5b81f72dddb
Dilshodbek Ilhomov <dilshodbekilhomov88@gmail.com>
2026-06-11 20:42:49 +0500

    created orders model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from users.models import User
+from books.models import Book
+
 
 # Create your models here.
+
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('cancelled', 'Cancelled')
+    )
+    user = models.ForeignKey(User, models.CASCADE, related_name='orders')
+    status = models.CharField(max_length=25, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Order #{self.id}'
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='order_items')
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.book.title} x {self.quantity}'
