fdb6e11a83ca1de9a1314697a01774f4f825e6d3
Dilshodbek Ilhomov <dilshodbekilhomov88@gmail.com>
2026-06-12 22:02:45 +0500

    created users serializer

---
+++ users/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from rest_framework.validators import UniqueValidator
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        read_only_fields = ['id', 'username', 'email']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    """
+    Serializer for registering a new user.
+    Includes password confirmation and validation.
+    """
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+        style={'input_type': 'password'}
+    )
+    password2 = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'}
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2', 'first_name', 'last_name']
+
+    def validate(self, attrs):
+        """
+        Ensure both passwords match.
+        """
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        """
+        Create a new user with hashed password.
+        """
+        validated_data.pop('password2')  # Remove password2 as it's not in the model
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
+        return user
