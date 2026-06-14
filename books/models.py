69dbb2da9e3f40e84d92d901edb23b6b9a5f8906
Dilshodbek Ilhomov <dilshodbekilhomov88@gmail.com>
2026-06-14 22:46:46 +0500

    added cover image and book file model fields on books app

--- books/models.py
+++ books/models.py
@@ -16,6 +16,8 @@ class Author(models.Model):
         return self.full_name
 
 class Book(models.Model):
+    cover_image = models.ImageField(upload_to='books/covers/')
+    book_file = models.FileField(upload_to='books/files/')
     title = models.CharField(max_length=155)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
