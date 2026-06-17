22997b9e0619f03c067a806a5ed3270d27c031d0
Dilshodbek Ilhomov <dilshodbekilhomov88@gmail.com>
2026-06-17 22:37:26 +0500

    added books views.py

--- books/views.py
+++ books/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Category, Author, Book
+from .serializers import CategorySerializer, AuthorSerializer, BookSerializer
+
 
 # Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = CategorySerializer
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = AuthorSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = BookSerializer
