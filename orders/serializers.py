8a28c988014329d0b2e20342bf66cbfafee491fd
Dilshodbek Ilhomov <dilshodbekilhomov88@gmail.com>
2026-06-26 18:54:11 +0500

    added create serializers

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from books.models import Book
 from .models import OrderItem, Order
 
 class OrderItemSerializer(serializers.ModelSerializer):
@@ -14,3 +15,44 @@ class OrderSerializer(serializers.ModelSerializer):
         fields = ['id', 'user', 'status', 'created_at', 'items']
 
     read_only_fields = ['id', 'created_at']
+
+class OrderItemCreateSerializer(serializers.Serializer):
+    book = serializers.IntegerField()
+    quantity = serializers.IntegerField(min_value=1)
+
+
+class OrderCreateSerializer(serializers.Serializer):
+    items = OrderItemCreateSerializer(many=True)
+
+    def create(self, validated_data):
+        request = self.context['request']
+        user = request.user
+
+        items_data = validated_data.pop('items')
+
+        order = Order.objects.create(
+            user=user,
+            status='pending'
+        )
+
+        for item in items_data:
+            book = Book.objects.get(id=item['book'])
+            quantity = item['quantity']
+
+            if book.stock < quantity:
+                raise serializers.ValidationError(
+                    f"{book.title} kitobidan yetarli miqdor mavjud emas."
+                )
+
+            OrderItem.objects.create(
+                order=order,
+                book=book,
+                quantity=quantity,
+                price=book.price,
+                total_price=book.price * quantity
+            )
+
+            book.stock -= quantity
+            book.save()
+
+        return order
