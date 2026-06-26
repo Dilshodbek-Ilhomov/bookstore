75814cb6eaa1275220862ade3177cf456de84042
Dilshodbek Ilhomov <dilshodbekilhomov88@gmail.com>
2026-06-26 23:30:14 +0500

    mini update

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,19 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .models import Order
-from .serializers import OrderSerializer
+from .serializers import OrderSerializer, OrderCreateSerializer
 
 # Create your views here.
 
 class OrderViewSet(ModelViewSet):
-    queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return OrderCreateSerializer
+
+        return OrderSerializer
