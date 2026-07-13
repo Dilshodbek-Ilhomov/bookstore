a97be608352f3824285a69f40027e5afffda5eb5
Dilshodbek Ilhomov <dilshodbekilhomov88@gmail.com>
2026-07-13 14:44:39 +0500

    fixed: detect HTTPS behind Nginx proxy

--- config/settings.py
+++ config/settings.py
@@ -142,3 +142,8 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
+
+SECURE_PROXY_SSL_HEADER = (
+    "HTTP_X_FORWARDED_PROTO",
+    "https",
+)
